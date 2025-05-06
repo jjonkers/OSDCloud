e5048de245e5f7585ee1ca3ee3dd484eb5254dd2
Jaap Jonkers <jonkers@jmdejonggroep.nl>
2025-05-06 16:43:16 +0200

    dd install hp image assistent

---
+++ Scripts/install-hp-image-assistent.ps1
@@ -0,0 +1,14 @@
+#download from HP 
+invoke-webrequest -URI "https://hpia.hpcloud.hp.com/downloads/hpia/hp-hpia-5.3.1.exe" -Outfile "c:\temp\hp-hpia-5.3.1.exe" 
+
+#run
+Start-Process C:\Temp\hp-hpia-5.3.1.exe -ArgumentList '/s /e /f "C:\Program Files (x86)\HP Image Assistant"' 
+
+#create ShortCut
+$TargetFile = "C:\Program Files (x86)\HP Image Assistant\HPImageAssistant.exe"
+$ShortcutFile = "$env:Beheerder\Desktop\HP Image Assistant.lnk" 
+#$ShortcutFile = "c:\temp\HP Image Assistant.lnk" 
+$WScriptShell = New-Object -ComObject WScript.Shell 
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile) 
+$Shortcut.TargetPath = $TargetFile 
+$Shortcut.Save()
