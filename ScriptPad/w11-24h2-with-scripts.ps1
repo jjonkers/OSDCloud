9e57b10bbf8bb8ed76e6cbf772376ebdd55fc395
Jaap Jonkers <jonkers@jmdejonggroep.nl>
2025-05-07 11:29:45 +0200

    Typo

--- ScriptPad/w11-24h2-with-scripts.ps1
+++ ScriptPad/w11-24h2-with-scripts.ps1
@@ -136,6 +136,7 @@ Invoke-RestMethod https://raw.githubusercontent.com/jjonkers/OSDCloud/refs/heads
 
 Invoke-RestMethod https://raw.githubusercontent.com/jjonkers/OSDCloud/refs/heads/main/Scripts/install-action1.ps1 | Out-file -FilePath 'c:\windows\setup\scripts\install-action1.ps1' -Encoding ascii -Force
 
+Invoke-RestMethod https://raw.githubusercontent.com/jjonkers/OSDCloud/refs/heads/main/Scripts/LayoutModification.ps1 | Out-file -FilePath 'c:\windows\setup\scripts\LayoutModification.ps1 ' -Encoding ascii -Force
 
 
 $OOBECMD = @'
@@ -147,6 +148,8 @@ $OOBECMD = @'
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\setup\scripts\AppxRemoval.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\setup\scripts\install-hp-image-assistent.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\setup\scripts\install-action1.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\setup\scripts\LayoutModification.ps1 
+
 
 # Below a PS session for debug and testing in system context, # when not needed 
 # start /wait powershell.exe -NoL -ExecutionPolicy Bypass
--- Scripts/LayoutModification.ps1
+++ Scripts/LayoutModification.ps1
@@ -5,4 +5,4 @@ If (!(Test-Path "C:\temp")) {
 }
 
 
-invoke-webrequest -URI https://raw.githubusercontent.com/jjonkers/OSDCloud/refs/heads/main/Scripts/LayoutModification.xml -OutFile "c:\users\default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml"
+invoke-webrequest -URI "https://raw.githubusercontent.com/jjonkers/OSDCloud/refs/heads/main/Scripts/LayoutModification.xml" -OutFile "c:\users\default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml"
