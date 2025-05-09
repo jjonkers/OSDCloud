ea57784dfe3ff2bf4ed7cb6312d0e4c0fd12ae48
Jaap Jonkers <jonkers@jmdejonggroep.nl>
2025-05-09 08:50:41 +0200

    add AVD Client

--- Scripts/install-chocolatly-packages.ps1
+++ Scripts/install-chocolatly-packages.ps1
@@ -1,1 +1,1 @@
-choco install adobereader googlechrome firefox 7zip.install notepadplusplus.install -y
+choco install adobereader googlechrome firefox 7zip.install notepadplusplus.install remote-desktop-client -y
