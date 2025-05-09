5a76eb45c84e7df2b3cc911931fb8289242c63a8
Jaap Jonkers <jonkers@jmdejonggroep.nl>
2025-05-09 10:43:53 +0200

    added full path

--- Scripts/install-chocolatly-packages.ps1
+++ Scripts/install-chocolatly-packages.ps1
@@ -1,1 +1,2 @@
-choco install adobereader googlechrome firefox 7zip.install notepadplusplus.install remote-desktop-client -y
+#install apps, include fullpath to chocolatey 
+c:\programdata\chocolatey\choco.exe install adobereader googlechrome firefox 7zip.install notepadplusplus.install remote-desktop-client -y
