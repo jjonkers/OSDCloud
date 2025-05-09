5dc635c255c1c96da27a9974501c4ac27bfafab1
Jaap Jonkers <jonkers@jmdejonggroep.nl>
2025-05-09 11:30:04 +0200

    Office 365 incl xml via Choco

--- Scripts/install-chocolatly-packages.ps1
+++ Scripts/install-chocolatly-packages.ps1
@@ -1,2 +1,11 @@
 #install apps, include fullpath to chocolatey 
-c:\programdata\chocolatey\choco.exe install adobereader googlechrome firefox 7zip.install notepadplusplus.install remote-desktop-client -y
+c:\programdata\chocolatey\choco.exe install adobereader googlechrome firefox 7zip.install notepadplusplus.install remote-desktop-client clickshare-desktop -y
+
+#download xml
+invoke-webrequest https://raw.githubusercontent.com/jjonkers/OSDcloud/refs/heads/main/Scripts/Office365_x64_Online.xml -outfile  c:\temp\Office365_x64_Online.xml
+
+#install Office365_x64_Online
+c:\programdata\chocolatey\choco.exe install office365business --params "/configpath:C:\temp\Office365_x64_Online.xml" --force -y
+
+#remove desktop icons 
+remove-item c:\users\public\desktop\*.lnk
