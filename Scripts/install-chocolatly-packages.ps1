b6ad27ab32d4b695140154bf9c49875f9c0fa385
Jaap Jonkers <jonkers@jmdejonggroep.nl>
2025-05-09 14:01:57 +0200

    VlC and Greenshot Please enter the commit message for your changes. Lines starting

--- Scripts/install-chocolatly-packages.ps1
+++ Scripts/install-chocolatly-packages.ps1
@@ -1,5 +1,5 @@
 #install apps, include fullpath to chocolatey 
-c:\programdata\chocolatey\choco.exe install adobereader googlechrome firefox 7zip.install notepadplusplus.install remote-desktop-client clickshare-desktop -y
+c:\programdata\chocolatey\choco.exe install adobereader googlechrome firefox 7zip.install notepadplusplus.install remote-desktop-client clickshare-desktop greenshot vlc -y
 
 #download xml
 invoke-webrequest https://raw.githubusercontent.com/jjonkers/OSDcloud/refs/heads/main/Scripts/Office365_x64_Online.xml -outfile  c:\temp\Office365_x64_Online.xml
