0929a55d485450060b9c36e746d6ee73780d1e25
Jaap Jonkers <jonkers@jmdejonggroep.nl>
2025-06-12 08:48:44 +0200

    added no-domain

---
+++ Scripts/install-chocolatly-packages-nodomain.ps1
@@ -0,0 +1,6 @@
+#install apps, include fullpath to chocolatey 
+c:\programdata\chocolatey\choco.exe install googlechrome firefox teamviewer --ignore-checksums -y
+
+
+#remove desktop icons 
+remove-item c:\users\public\desktop\*.lnk
